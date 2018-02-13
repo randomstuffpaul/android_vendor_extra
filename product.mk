a49ab88dde23d60afd37a4814ed87943a1f18f0e
Paul Keith <javelinanddart@gmail.com>
2018-02-13 15:45:05 +0100

    Clean things I don't want

--- product.mk
+++ product.mk
@@ -1,24 +1,2 @@
+# Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-PRODUCT_PACKAGES += auditd
-
-$(call prepend-product-if-exists, vendor/invisiblek/product.mk)
-
-ifneq ($(filter lineage_marlin lineage_mata lineage_nash lineage_sailfish,$(TARGET_PRODUCT)),)
-GAPPS_VARIANT := nano
-$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
-
-PRODUCT_COPY_FILES += \
-  vendor/opengapps/sources/arm/lib/23/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-endif
-
-TARGET_EXTRA_KERNEL_MODULES := wireguard
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/rootdir/etc/init/ttl.rc:system/etc/init/ttl.rc
-
-# Themes
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
-PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
-PRODUCT_PACKAGES += \
-    PixelTheme
