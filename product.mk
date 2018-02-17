51a0da4cc430e9f04fab2ca0289011346510d9b1
Paul Keith <javelinanddart@gmail.com>
2018-02-17 04:04:01 +0100

    Dirty hax for including gapps

--- product.mk
+++ product.mk
@@ -1,2 +1,12 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+
+# Google Apps
+ifeq ($(WITH_GMS), true)
+ifeq ($(TARGET_PRODUCT), lineage_ether)
+TARGET_ARCH := arm64
+else
+TARGET_ARCH := arm
+endif
+-include vendor/gapps/$(TARGET_ARCH)/$(TARGET_ARCH)-vendor.mk
+endif
