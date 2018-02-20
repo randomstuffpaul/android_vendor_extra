ae96183be53d34d47ab8ec8ec650468ad216ac27
Paul Keith <javelinanddart@gmail.com>
2018-02-20 15:20:51 +0100

    Add some more flags from my bashrc
    * No point setting these on a bashrc level

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,2 +1,8 @@
 # Use Snapdragon LLVM if available on build server
 TARGET_USE_SDCLANG := true
+
+# Append time of day to compiled build zip
+LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+ # Include GApps
+WITH_GMS := true
