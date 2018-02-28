0210232c442373e5b08a32735b63389b36fc8ad7
Paul Keith <javelinanddart@gmail.com>
2018-02-28 15:46:51 +0100

    Add some more env vars

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,8 +1,15 @@
 # Use Snapdragon LLVM if available on build server
 TARGET_USE_SDCLANG := true
 
+## Environment variables, setting this here DOES NOT WORK
+# Don't use jack
+ANDROID_COMPILE_WITH_JACK := false
+
 # Append time of day to compiled build zip
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
 
  # Include GApps
 WITH_GMS := true
+
+# Include SU
+WITH_SU := true
