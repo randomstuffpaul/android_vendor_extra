144aa09c617ba3b246c7aa1e48eae20ec2601f70
Paul Keith <javelinanddart@gmail.com>
2018-04-06 19:54:36 +0200

    Cleanups for ether removal

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -4,17 +4,12 @@ TARGET_USE_SDCLANG := true
 # Don't dexpreopt apps
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
-# Fix ADB on ether
-ifeq ($(TARGET_PRODUCT), lineage_ether)
-TARGET_NEEDS_HACKED_VENDORID := true
-endif
+ # Include GApps
+WITH_GMS := true
 
 ## Environment variables, setting this here DOES NOT WORK
 # Append time of day to compiled build zip
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
-
- # Include GApps
-WITH_GMS := true
 
 # Include SU
 WITH_SU := true
--- product.mk
+++ product.mk
@@ -3,24 +3,17 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 # Google Apps
 ifeq ($(WITH_GMS), true)
-ifeq ($(TARGET_PRODUCT), lineage_ether)
-TARGET_ARCH := arm64
+ifeq ($(filter lineage_arm64,$(PRODUCT_NAME)),)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 else
-TARGET_ARCH := arm
+$(call inherit-product, vendor/gapps/arm/arm-vendor.mk)
 endif
--include vendor/gapps/$(TARGET_ARCH)/$(TARGET_ARCH)-vendor.mk
 endif
 
 # Volume Steps
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=7 \
     ro.config.media_vol_steps=25
-
-# Default to 45 FPS
-ifeq ($(TARGET_PRODUCT), lineage_ether)
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.display.frame_rate=1
-endif
 
 # Recovery ADB keys
 PRODUCT_COPY_FILES += \
