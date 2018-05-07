8b87f64192e6d624bb5fa244d8a484a7c2f31ff7
Paul Keith <javelinanddart@gmail.com>
2018-05-08 01:12:06 +0200

    Setup env vars properly

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -3,13 +3,3 @@ TARGET_USE_SDCLANG := true
 
 # Don't dexpreopt apps
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-
- # Include GApps
-WITH_GMS := true
-
-## Environment variables, setting this here DOES NOT WORK
-# Append time of day to compiled build zip
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
-
-# Include SU
-WITH_SU := true
