b30daaa277915b043a4859d27a69af9785b2bb10
Paul Keith <javelinanddart@gmail.com>
2018-07-26 04:56:36 +0200

    biqqles-ify
    * Remove my adb_keys
    * Remove berkeley stuff
    * Remove gapps, backuptool isneeded
    * Remove squircle
    * Enable round icons
    * Remove USB hacks
    * Remove SU, safetynet is desired

--- product.mk
+++ product.mk
@@ -1,32 +1,7 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
-# Google Apps
-ifeq ($(WITH_GMS), true)
-ifeq ($(filter lineage_berkeley lineage_ether,$(PRODUCT_NAME)),)
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
-else
-$(call inherit-product, vendor/gapps/arm/arm-vendor.mk)
-endif
-endif
-
-ifeq ($(filter lineage_ether,$(PRODUCT_NAME)),)
-PRODUCT_PACKAGES += \
-    init.jav.usb.rc
-endif
-
-# Huawei stuff
-ifeq ($(filter lineage_berkeley,$(PRODUCT_NAME)),)
-PRODUCT_PACKAGES += \
-    HotwordEnrollmentOKGoogleHI6403 \
-    HotwordEnrollmentXGoogleHI6403
-endif
-
 # Volume Steps
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=7 \
     ro.config.media_vol_steps=25
-
-# Recovery ADB keys
-PRODUCT_COPY_FILES += \
-    vendor/extra/adb_keys:recovery/root/adb_keys
