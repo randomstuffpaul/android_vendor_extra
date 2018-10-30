935e64a0bdee85f7f46bceabfb8ca343c4d3db8e
Paul Keith <javelinanddart@gmail.com>
2018-10-30 05:00:57 +0100

    Revamp
    * Back to circle icons across the board
    * Nuke ether stuff
    * Add gapps back (A/B, I hate you)
    * Add signing back

--- product.mk
+++ product.mk
@@ -3,21 +3,17 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 # Google Apps
 ifeq ($(WITH_GMS), true)
-ifeq ($(filter lineage_berkeley lineage_ether,$(PRODUCT_NAME)),)
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
-else
-$(call inherit-product, vendor/gapps/arm/arm-vendor.mk)
-endif
 endif
 
 # Signing
-#PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
-#PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
 
-ifeq ($(filter lineage_ether,$(PRODUCT_NAME)),)
-PRODUCT_PACKAGES += \
-    init.jav.usb.rc
-endif
+#ifeq ($(filter lineage_ether,$(PRODUCT_NAME)),)
+#PRODUCT_PACKAGES += \
+#    init.jav.usb.rc
+#endif
 
 # Huawei stuff
 #ifeq ($(filter lineage_berkeley,$(PRODUCT_NAME)),)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-#export WITH_GMS=true
+export WITH_GMS=true
 #export WITH_SU=true
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 export CM_VERSION_APPEND_TIME_OF_DAY=true
