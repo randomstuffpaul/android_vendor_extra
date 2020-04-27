8b27be052d3928bbd85189afe351c5a3ee553e69
Paul Keith <javelinanddart@gmail.com>
2020-04-26 22:46:44 -0500

    Update for 17.1
    * Remove dead/redundant flags
    * Move to luca gapps

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,8 +1,2 @@
-# Use Snapdragon LLVM if available on build server
-TARGET_USE_SDCLANG := true
-
 # Don't dexpreopt apps
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-
-# Filesystem
-TARGET_USES_MKE2FS := true
--- product.mk
+++ product.mk
@@ -3,17 +3,8 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 # Google Apps
 ifeq ($(WITH_GMS), true)
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 endif
-
-# Signing
-#PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
-#PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
-
-# Volume Steps
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.vc_call_vol_steps=7 \
-    ro.config.media_vol_steps=25
 
 # Recovery ADB keys
 PRODUCT_COPY_FILES += \
