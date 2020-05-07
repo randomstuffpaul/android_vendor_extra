b309349c1553511e408c999d9ebdd51a7c615561
Paul Keith <javelinanddart@gmail.com>
2020-05-06 17:13:43 -0700

    Don't include arm64 gapps on hlte

--- product.mk
+++ product.mk
@@ -3,7 +3,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 # Google Apps
 ifeq ($(WITH_GMS), true)
+ifneq ($(filter lineage_hlte,$(PRODUCT_NAME)),)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+endif
 endif
 
 # Recovery ADB keys
